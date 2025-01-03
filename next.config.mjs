2ab1a925fcb4c929c01a9400c32e25e4e407e02e
NFTGameA <joehirest72@gmail.com>
2025-01-03 16:21:27 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src')
+    }
+    return config
+  }
+};
 
 export default nextConfig;
